7ae44394b8e3b7f22b05670b6d28336a45232bfd
SN Lucas <lucas.lambda.101@gmail.com>
2021-02-25 01:05:51 -0300

    Change the clock package and creates a countdown clock

--- flutter_pomodoro_timer/lib/home_page.dart
+++ flutter_pomodoro_timer/lib/home_page.dart
@@ -4,21 +4,51 @@ import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Clock Dimensions
+    final double height = MediaQuery.of(context).size.height / 2;
+    final double width = MediaQuery.of(context).size.width / 2;
+
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.headset_off),
+        ),
+        title: Text('Pomodoro'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.alarm_off),
+          ),
+        ],
+      ),
       body: Container(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Column(
             children: <Widget>[
               Center(
+                child: Container(
+                  padding: EdgeInsets.all(20.0),
+                  height: MediaQuery.of(context).size.height / 2.75,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(100.0),
+                  ),
                   child: CircularCountDownTimer(
-                duration: 10,
-                isReverse: true,
-                height: MediaQuery.of(context).size.height / 2,
-                width: MediaQuery.of(context).size.width / 2,
-                ringColor: Colors.blue[900],
-                fillColor: Colors.pink,
-              )),
+                    duration: 10 * 60,
+                    isReverse: true,
+                    textStyle: TextStyle(color: Colors.white),
+                    height: height,
+                    width: width,
+                    ringColor: Color(0xff0B0C19),
+                    fillColor: Colors.pink,
+                    backgroundColor: Color(0xFF2A2B4D),
+                    strokeCap: StrokeCap.round,
+                  ),
+                ),
+              ),
             ],
           ),
         ),
