3138a63ada4624bdca6888a14fa715399b241db9
SN Lucas <lucas.lambda.101@gmail.com>
2021-02-26 15:27:59 -0300

    Add Text constants

--- flutter_pomodoro_timer/lib/utils/constants.dart
+++ flutter_pomodoro_timer/lib/utils/constants.dart
@@ -8,3 +8,8 @@ const Icon kPauseClockButton = Icon(Icons.pause_sharp);
 const int kWorkDuration = 25;
 const int kShortBreakDuration = 5;
 const int kLongBreakDuration = 30;
+
+// Text constants
+const String kWorkLabel = 'Work';
+const String kShortBreakLabel = 'Short break';
+const String kLongBreakLabel = 'Long break';
