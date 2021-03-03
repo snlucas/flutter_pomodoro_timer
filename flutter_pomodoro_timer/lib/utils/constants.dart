404fa77dc6b4c4b32d703e5dae6b8dd055724648
lucas <lucas.lambda.101@gmail.com>
2021-03-02 21:41:43 -0300

    Fix constants file

--- flutter_pomodoro_timer/lib/utils/constants.dart
+++ flutter_pomodoro_timer/lib/utils/constants.dart
@@ -5,9 +5,6 @@ const Icon kPlayClockButton = Icon(Icons.play_arrow_sharp);
 const Icon kPauseClockButton = Icon(Icons.pause_sharp);
 
 // Time constants
-// const int kWorkDuration = 25;
-// const int kShortBreakDuration = 5;
-// const int kLongBreakDuration = 30;
-const int kWorkDuration = 1;
-const int kShortBreakDuration = 1;
-const int kLongBreakDuration = 1;
+const int kWorkDuration = 25;
+const int kShortBreakDuration = 5;
+const int kLongBreakDuration = 30;
